c7b92053c12aeecdf7cc3820b4594dfb3cb9c4e6
s24586-pj <92204712+s24586-pj@users.noreply.github.com>
2022-04-02 17:06:56 +0200

    Add files via upload

---
+++ TrojkatRownoboczny.java
@@ -0,0 +1,16 @@
+public class TrojkatRownoboczny extends TrojkatRownoramienny {
+    public TrojkatRownoboczny(double podstawa, double podstawa1) {
+    super(podstawa ,podstawa);
+    }
+
+    public TrojkatRownoboczny(int podstawa) {
+        super(podstawa,podstawa);
+
+
+    }
+
+    public double wysokosc() {
+  return 0;
+
+    }
+}
---
+++ TrojkatRownoramienny.java
@@ -0,0 +1,19 @@
+public class TrojkatRownoramienny {
+
+    public TrojkatRownoramienny(double podstawa, double ramie) {
+
+    }
+
+
+    public double wysokosc(double i) {
+
+
+        return i;
+    }
+
+    public double wysokosc() {
+    return wysokosc();
+    }
+}
+
+
